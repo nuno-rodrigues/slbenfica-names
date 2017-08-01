0787fecf94cf2e4a6234f64d6cd90da54653dce0
e-nrrodrigues@tap.pt <e-nrrodrigues@tap.pt>
2017-08-01 15:28:31 +0100

    feat(random): Add the ability to get an array of slbenfica names
    If you pass a number to the random function, you will receive an array with a number of random items
    
    closes #2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 var uniqueRandomArray = require('unique-random-array');
 var slBenficaNames = require('./slbenfica-names.json');
+var getRandomItem = uniqueRandomArray(slBenficaNames);
 
 module.exports = {
     all: slBenficaNames,
-    random: uniqueRandomArray(slBenficaNames)
+    random: random
 };
+
+function random(number) {
+    if (number === undefined) {
+        return getRandomItem();
+    } else {
+        var randomItems = [];
+        for (var i = 0; i < number; i++) {
+            randomItems.push(getRandomItem());
+        }
+        return randomItems;
+    }
+}
--- src/index.test.js
+++ src/index.test.js
@@ -28,5 +28,13 @@ describe('slbenfica-names', function() {
             var randomItem = slBenficaNames.random();
             expect(slBenficaNames.all).to.include(randomItem);
         })
+
+        it('should return an array of items if passed a number', function() {
+            var randomItems = slBenficaNames.random(3);
+            expect(randomItems).to.have.length(3);
+            randomItems.forEach(function(item) {
+                expect(slBenficaNames.all).to.include(item);
+            })
+        })
     })
 })
